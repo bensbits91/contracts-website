f469b69f794ab1655825a36e1199d76bee9204ad
bensbits91 <benbpnw@gmail.com>
2025-03-14 16:23:30 -0700

    add Card component; add Why page and some data

---
+++ app/why/page.js
@@ -0,0 +1,33 @@
+import { Section } from '@/app/components/layout';
+import { Card } from '@/app/components/card';
+import { why } from '@/app/data/why';
+import styles from './page.module.css';
+
+const Why = () => {
+   return (
+      <>
+         <Section>
+            <h1>Why choose Ben?</h1>
+         </Section>
+         <Section height='sm'>
+            <div className={styles.grid}>
+               {why.map((item, index) => (
+                  <Card key={index} heading={item.heading} headingLevel='2'>
+                     {typeof item.content === 'string' ? (
+                        <p>{item.content}</p>
+                     ) : (
+                        <ul>
+                           {item.content.map((content, index) => (
+                              <li key={index}>{content}</li>
+                           ))}
+                        </ul>
+                     )}
+                  </Card>
+               ))}
+            </div>
+         </Section>
+      </>
+   );
+};
+
+export default Why;
