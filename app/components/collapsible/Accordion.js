6c555faa07ddf0536436f395dc17a399faac0620
bensbits91 <benbpnw@gmail.com>
2025-03-15 14:08:12 -0700

    add animations to global.css; add Modal, Overlay, Form and Thanks components; finish FAQ, ask new question

--- app/components/collapsible/Accordion.js
+++ app/components/collapsible/Accordion.js
@@ -2,7 +2,10 @@
 import { useEffect, useState } from 'react';
 import Collapsible from './Collapsible';
 import { Heading } from '@/app/components/typography';
-import { TextInput, Toggle } from '@/app/components/inputs';
+import { Button, TextInput, Toggle } from '@/app/components/inputs';
+import { Modal } from '@/app/components/modal';
+import { Form } from '@/app/components/form';
+import { Thanks } from '@/app/components/thanks';
 import styles from './Accordion.module.css';
 
 const Accordion = ({ collapsibleItems }) => {
@@ -21,10 +24,13 @@ const Accordion = ({ collapsibleItems }) => {
    const [filteredItems, setFilteredItems] = useState(collapsibleItems);
    const handleSearch = e => setSearchTerm(e.target.value);
 
+   const [modalData, setModalData] = useState(null);
+   const handleCloseModal = () => setModalData(null);
+
    useEffect(() => {
       if (searchTerm) {
          const newFilteredItems = collapsibleItems.filter(item => {
-            return item.q.toLowerCase().includes(searchTerm.toLowerCase());
+            return JSON.stringify(item).toLowerCase().includes(searchTerm.toLowerCase());
          });
          setFilteredItems(newFilteredItems);
       } else {
@@ -51,49 +57,111 @@ const Accordion = ({ collapsibleItems }) => {
       );
    }, [openItems, collapsibleItems.length]);
 
+   const handleQuestionSubmit = formData => {
+      console.log('formData', formData);
+      // send to server
+      // if success,
+      setModalData({ headingText: 'Thanks!', hasForm: false });
+   };
+
+   const handleAskAnotherQuestion = () => {
+      setModalData({ headingText: 'Ask another question', hasForm: true });
+   };
+
    return (
-      <div className={styles.accordion}>
-         <div className={styles.headWrap}>
-            <div className={styles.textWrap}>
-               <Heading level='2' color='blue'>
-                  Frequently Asked Questions
-               </Heading>
+      <>
+         <div className={styles.accordion}>
+            <Heading level='2' color='blue'>
+               Frequently Asked Questions
+            </Heading>
+            <div className={styles.toolbar}>
+               {/* <div className={styles.textWrap}>
                <p>Some frequently asked questions :)</p>
-            </div>
-            <div className={styles.searchWrap}>
-               <TextInput
-                  label='Search'
-                  type='search'
-                  name='search'
-                  required={false}
-                  placeholder='Search'
-                  onChange={handleSearch}
-               />
-            </div>
-            <div className={styles.toggleWrap}>
-               <Toggle
-                  isOn={allOpen}
-                  offText='Open all'
-                  onText='Close all'
-                  onChange={toggleAll}
-               />
-            </div>
-         </div>
-         {filteredItems.map((item, index) => {
-            const { q, a } = item;
-            const originalIndex = collapsibleItems.indexOf(item);
-            return (
-               <div key={originalIndex}>
-                  <Collapsible
-                     title={q}
-                     content={a}
-                     isOpen={openItems[originalIndex] || false}
-                     onToggle={() => toggleItem(originalIndex)}
+            </div> */}
+               <div className={styles.searchWrap}>
+                  <TextInput
+                     label='Search'
+                     type='search'
+                     name='search'
+                     required={false}
+                     placeholder='Search'
+                     onChange={handleSearch}
                   />
                </div>
-            );
-         })}
-      </div>
+               <div className={styles.toggleWrap}>
+                  <Toggle
+                     isOn={allOpen}
+                     offText='Open all'
+                     onText='Close all'
+                     onChange={toggleAll}
+                  />
+               </div>
+               <div className={styles.buttonWrap}>
+                  <Button
+                     onClick={() =>
+                        setModalData({ headingText: 'Ask a new question', hasForm: true })
+                     }>
+                     Ask a new question
+                  </Button>
+               </div>
+            </div>
+            {filteredItems.map(item => {
+               const { q, a } = item;
+               const originalIndex = collapsibleItems.indexOf(item);
+               return (
+                  <div key={originalIndex}>
+                     <Collapsible
+                        title={q}
+                        content={a}
+                        isOpen={openItems[originalIndex] || false}
+                        onToggle={() => toggleItem(originalIndex)}
+                     />
+                  </div>
+               );
+            })}
+         </div>
+         {modalData && (
+            <Modal
+               modalData={modalData}
+               //    actions={modalActions}
+               handleCloseModal={handleCloseModal}>
+               {modalData.hasForm ? (
+                  <Form
+                     fields={[
+                        {
+                           type: 'email',
+                           name: 'Your email',
+                           required: true
+                        },
+                        {
+                           type: 'textarea',
+                           label: 'Your question',
+                           placeholder: 'Type your question here',
+                           name: 'Your question',
+                           required: true
+                        },
+                        {
+                           name: 'Submit',
+                           type: 'submit'
+                        }
+                     ]}
+                     handleForm={handleQuestionSubmit}
+                  />
+               ) : (
+                  <Thanks
+                     content='Thanks for your question!'
+                     buttons={[
+                        {
+                           text: 'Ask another question',
+                           onClick: handleAskAnotherQuestion
+                        },
+                        { text: 'Done', onClick: handleCloseModal }
+                     ]}
+                  />
+               )}
+            </Modal>
+         )}
+      </>
    );
 };
 
